ebf912d2dbe5252cf2e2efe8fb0a7984fc9f05e1
alex-rbn <Aleksandr.rbn@yandex.by>
2022-10-25 04:02:20 +0300

    app 1.3

--- dj_site/settings.py
+++ dj_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n)up1k!*ctc6!=^vv&fum3q5+n7&*+ef51f_b1mikcgtcsi#(0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- riddles/urls.py
+++ riddles/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     re_path(r'^login/$', LoginFormView.as_view()),
     re_path(r'^logout/$', LogoutView.as_view()),
     re_path(r'^password-change/', PasswordChangeView.as_view()),
+    re_path(r'^([0-9]+)/post/$', post, name='post'),
     re_path(r'^([0-9]+)/msg_list/$', msg_list, name='msg_list'),
     re_path(r'^([0-9]+)/post_mark/$', post_mark, name='post_mark'),
     re_path(r'^([0-9]+)/get_mark/$', get_mark, name='get_mark'),
